158b5aec1d29abb9fd7c4f269c8c52bcbc80e56e
Nielle <ythevoz@outlook.com>
2023-07-01 11:17:56 +0200

    Button with voiceover

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    let synthesizer = AVSpeechSynthesizer()
+        
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Stay Hungry, Stay Foolish")
+                .fontWeight(.heavy)
+                .font(.system(.title, design: .rounded))
         }
         .padding()
+        
+        Button {
+            let utterance = AVSpeechUtterance(string: "Hello World")
+            utterance.voice = AVSpeechSynthesisVoice(language: "fr-CH")
+            
+            synthesizer.speak(utterance)
+            // What to perform
+        } label: {
+            // How the button looks like
+            Text("Hello World")
+                            .fontWeight(.bold)
+                            .font(.system(.title, design: .rounded))
+        }
+        .foregroundColor(.white)
+        .background(Color.purple)
+        .cornerRadius(20)
     }
 }
 
